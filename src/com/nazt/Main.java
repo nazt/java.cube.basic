172c9eae035acb5bdf0a7548ad0b5a482aa31da0
Nat <nat.weerawan@gmail.com>
2010-09-11 01:02:36 +0700

    First Commit

---
+++ src/com/nazt/Main.java
@@ -0,0 +1,18 @@
+package com.nazt;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Human a1 = new Human("AName", "ASurname");
+		Student nat = new Student("Nat", "Weerawan", "4909660161");
+		
+		a1.introduce();
+		nat.introduce();
+	}
+
+}
+	
