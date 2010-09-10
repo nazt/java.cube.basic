816adcbc67f2275f79c3a7ab173954c3efec57b2
Nat <nat.weerawan@gmail.com>
2010-09-11 01:18:52 +0700

    Refactor Triangle to Square

---
+++ src/com/nazt/Square.java
@@ -0,0 +1,17 @@
+package com.nazt;
+
+public class Square implements Shape {
+
+	@Override
+	public double getArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getCircumference() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
